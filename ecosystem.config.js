5224790e0572df47d83393fb936f8e8ef004e4c0
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2017-08-19 16:01:55 +0900

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,43 @@
+var user = encodeURIComponent(process.env.GITHUB_USER);
+var password = encodeURIComponent(process.env.GITHUB_PASSWORD);
+var keyPath = process.env.SSH_PRIVATE_KEY_PATH || "~/.ssh/id_rsa";
+
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    // First application
+    {
+      name: "hello-world",
+      script: "npm",
+      args: "start",
+      env: {
+        PORT: 3003
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      key: keyPath,
+      user: "eiurur",
+      host: [process.env.SSH_HOST],
+      port: [process.env.SSH_PORT],
+      ref: "origin/master",
+      repo: `https://${user}:${password}+@github.com/kaziba/experess-wercker-pm2-deploy.git`,
+      ssh_options: "StrictHostKeyChecking=no",
+      path: "/home/eiurur/experess-wercker-pm2-deploy",
+      "post-setup": "npm install",
+      "post-deploy": "pm2 restart ecosystem.config.js --env production"
+    }
+  }
+};
